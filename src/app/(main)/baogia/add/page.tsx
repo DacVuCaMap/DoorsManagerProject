5b99b0aaaf3c7e117f1f86f9867b31f62ea6c94e
DacVuCaMap <namvu04112k@gmail.com>
2025-01-14 19:04:51 +0700

    up

--- src/app/(main)/baogia/add/page.tsx
+++ src/app/(main)/baogia/add/page.tsx
@@ -1,24 +1,74 @@
 
 import GetPattern from '@/ApiPattern/GetPattern';
-import AddBaoGia from '@/components/baogiaComponents/AddBaoGia'
 import CreateBaoGia from '@/components/NewBaoGiaComponents/CreateBaoGia';
-import { dataSelect } from '@/data/AddData'
 import { LoadAccesoryGroupNoAcs, LoadAccessoriesDataOffline, LoadFireTest, LoadListDoorModelData, LoadSelectData } from '@/data/FunctionAll';
 import Accessories from '@/Model/Accessories';
-import cmdData from '@/Model/cmdData';
-import DoorModel from '@/Model/DoorModel';
-import DoorNameSelect from '@/Model/DoorNameSelect';
-import FireTest from '@/Model/FireTest';
+import AcsAndType from '@/Model/AcsAndType';
 import GroupAccessory from '@/Model/GroupAccessory';
 import React from 'react'
 
 export default async function page() {
-  const groupAcsData : GroupAccessory[] = await LoadAccesoryGroupNoAcs();
-  const acsData : Accessories[] = await LoadAccessoriesDataOffline();
-  const doorModelData : any[] = await LoadListDoorModelData();
+
+  const LoadAccesoryGroupNoAcsTest = async () => {
+    let url = process.env.NEXT_PUBLIC_API_URL + "/api/accessories/get-list-group"
+    const response = await GetPattern(url, {});
+    if (response && response.value && Array.isArray(response.value)) {
+      const rs: GroupAccessory[] = response.value.map((item: any, index: number) => {
+        const acsAndType: AcsAndType[] = item.accessories.map((acs: any) => {
+          return { accessories: acs, type: acs.type };
+        })
+        return { id: item.id, name: item.name, type: item.type, accessoriesAndType: acsAndType };
+      })
+      return rs;
+    }
+    return [];
+  }
+  const LoadAccessoriesDataOfflineTest = async () => {
+    let url = process.env.NEXT_PUBLIC_API_URL + "/api/accessories/list?page=0&size=1000&search";
+    const response = await GetPattern(url, {});
+    if (response && response.content && Array.isArray(response.content)) {
+      const list: any[] = response.content;
+      let newAcs: Accessories[] = list.map((item: any, index: number) => {
+        return {
+          id: item.id,
+          code: item.code,
+          name: item.name,
+          supplier: item.supplier,
+          totalQuantity: 0,
+          quantity: 0,
+          width: 0,
+          height: 0,
+          orgPrice: item.orgPrice,
+          lowestPricePercent: item.lowestPricePercent,
+          lowPercent: item.lowPercent,
+          price: 0,
+          unit: item.unit,
+          status: false,
+          type: item.type,
+          isCommand: false,
+          accessoryGroup: item.accessoryGroup ? item.accessoryGroup : null,
+          acsDes: item.acsDes
+        };
+      });
+      return newAcs;
+    }
+    return [];
+  }
+  const LoadListDoorModelDataTest = async () => {
+    const url = process.env.NEXT_PUBLIC_API_URL + "/api/door-model/list";
+    const response = await GetPattern(url, {});
+    if (response && Array.isArray(response) && response.length > 0 && response[0].name) {
+      return response;
+    }
+    return [];
+  }
+
+  const groupAcsData: GroupAccessory[] = await LoadAccesoryGroupNoAcsTest();
+  const acsData: Accessories[] = await LoadAccessoriesDataOfflineTest();
+  const doorModelData: any[] = await LoadListDoorModelDataTest();
   return (
     <div className='w-full'>
-      {/* <AddBaoGia dataName={data}/> */} 
+      {/* <AddBaoGia dataName={data}/> */}
       <CreateBaoGia groupAcsData={groupAcsData} acsData={acsData} doorModelData={doorModelData} />
     </div>
   )
