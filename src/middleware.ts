6541665faa33b59571f294f4f1b9429fafaa379a
DacVuCaMap <namvu04112k@gmail.com>
2025-01-16 13:16:13 +0700

    up

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,11 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  const token = request.cookies.get('auth_token')
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more
